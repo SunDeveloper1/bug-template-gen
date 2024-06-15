6166b5c89494172e4324fff3daf68f1ab8881634
SunDeveloper1 <harikumar01947@gmail.com>
2024-06-16 00:01:26 +0530

    landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import LandingPage from "@/components/component/LandingPage";
+
+// import  HomePage  from "@/components/component/HomePage";
 
 export default function Home() {
   return (
     <div>
-      
+      <LandingPage/>
     </div>
   );
 }
