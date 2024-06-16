37421cf0a4db7e9d8cb1b5b4e5f0b61287a400eb
SunDeveloper1 <harikumar01947@gmail.com>
2024-06-16 13:26:23 +0530

    Added Mongodb

--- src/app/api/submit/route.js
+++ src/app/api/submit/route.js
@@ -1,18 +1,64 @@
 import {NextRequest, NextResponse} from "next/server";
+import { MongoClient } from "mongodb"
+
+let client =null;
+let database=null;
+let collection =null;
+
+const DBURI='mongodb+srv://dbuser:4NR6bc8vhMXCYSxP@bugdb.ahrf5dw.mongodb.net/?retryWrites=true&w=majority&appName=BugDBzzzzz'
+
+function generateBugId() {
+    // Generate a random number between 1000 and 9999
+    const randomNumber = Math.floor(1000 + Math.random() * 9000);
+    // Combine the prefix with the random number
+    const bugId = `BUG-${randomNumber}`;
+    return bugId;
+}
+
+const connectDB=async()=>{
+    try{
+        if(client == null){
+            client= new MongoClient(DBURI);
+            await client.connect();
+            console.log("Mongoose client connected")
+        }
+        if(database == null){
+            database = client.db("btg-db");
+        }
+        if(collection == null){
+            collection = database.collection("bugs");
+        }
+        return true
+
+    }catch(e){
+        console.error("error connecting database")
+        console.error(e.message)
+        return false;
+    }
+    
+}
+
 
 export async function GET (request){
-    const greeting = "Hello World!!"
-    const json = {
-        greeting
-    };
-    
-    return NextResponse.json(json);
+  
+   let dbConnected=await connectDB();
+     if(!dbConnected) return NextResponse.json({"message":"Something Went Wrong"}).status(400)  
+
+    const data= await collection.find({}).toArray();
+
+    return NextResponse.json({
+    "data":data
+    })
 }
 
 export async function POST(request){
   
+     let dbConnected=await connectDB();
+     if(!dbConnected) return NextResponse.json({"message":"Something Went Wrong"}).status(400)   
     const data=await request.json();
+    data.bugId=generateBugId()
+    const res=  await collection.insertMany([data])
+    console.log('res',res)
     console.log("data",data)
-
     return NextResponse.json(data);
 }
